cb1487de6a002dfd0de536c65ac7968e27502c62
chramakrishna1 <ramakrishna.chandakaa@gmail.com>
2025-11-24 14:00:38 +0530

    Add files via upload
    Js Assignment 1

---
+++ script.js
@@ -0,0 +1,50 @@
+
+
+
+
+  
+
+
+
+
+if (avg>90){
+    grade='A+'
+    console.log(grade)
+}
+else if (avr<=80 && avg<89){
+    grade='A';
+    console.log(grade);
+}
+
+
+else if (avr<=70 && avg<79){
+    grade='b';
+    console.log(grade);
+}
+
+else if (avr<=60 && avg<69){
+    grade='c';
+    console.log(grade);
+}
+
+else if (avr<=50 && avg<59){
+    grade='d';
+    console.log(grade);
+}
+
+else(avg<50){
+    grade='f'
+    console.log(grade)
+}
+
+
+
+
+
+
+
+
+
+
+
+
